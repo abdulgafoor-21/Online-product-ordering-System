09563ac34a75cade325c23578275aaf5d17d5007
abdulgafoor-21 <91531713+abdulgafoor-21@users.noreply.github.com>
2023-08-12 20:19:34 +0530

    Create ProductList.js

---
+++ Components/ProductList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const products = [
+  { id: 1, name: 'Product 1', price: 10.99 },
+  { id: 2, name: 'Product 2', price: 19.99 },
+  // Add more products here
+];
+
+function ProductList({ addToCart }) {
+  return (
+    <div className="product-list">
+      <h2>Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <span>{product.name}</span>
+            <span>${product.price}</span>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
